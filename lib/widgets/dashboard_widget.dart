237b3654c80ab5cfdbfff87d54ce77fa12ca9498
gitsudhanshur <sudhanshuranjan715@gmail.com>
2024-05-11 00:14:04 +0530

    first commit

---
+++ lib/widgets/dashboard_widget.dart
@@ -0,0 +1,34 @@
+import 'package:dashboard_project/util/responsive.dart';
+import 'package:dashboard_project/widgets/activity_details_card.dart';
+import 'package:dashboard_project/widgets/bar_graph_widget.dart';
+import 'package:dashboard_project/widgets/header_widget.dart';
+import 'package:dashboard_project/widgets/line_chart_card.dart';
+import 'package:dashboard_project/widgets/summary_widget.dart';
+import 'package:flutter/material.dart';
+
+class DashboardWidget extends StatelessWidget {
+  const DashboardWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 18),
+        child: Column(
+          children: [
+            const SizedBox(height: 18),
+            const HeaderWidget(),
+            const SizedBox(height: 18),
+            const ActivityDetailsCard(),
+            const SizedBox(height: 18),
+            const LineChartCard(),
+            const SizedBox(height: 18),
+            const BarGraphCard(),
+            const SizedBox(height: 18),
+            if (Responsive.isTablet(context)) const SummaryWidget(),
+          ],
+        ),
+      ),
+    );
+  }
+}
